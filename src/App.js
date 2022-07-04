b28564a87c69e621fdf73a0bac86daca7bfe4046
fatban <fnbz3010@gmail.com>
2022-07-04 13:12:15 +0100

    createLogic Operation

--- src/App.js
+++ src/App.js
@@ -1,60 +1,140 @@
 import React, { useState } from "react";
 import "./App.css";
-import Number from "./components/Number";
+import "./components/Number/style.css";
+import "./components/Operation/style.css";
+//import Number from "./components/Number";
 //import Calculator from './components/Number';
-import Operation from "./components/Operation";
-
+//import Operation from "./components/Operation";
 
 const data = [9, 1, 2, 3, 4, 5, 6, 7, 8, 0];
+//const operation = ["+", "-", "*", "=", "/", "del"];
+const operation = ["+", "-", "*", "/"];
 
 const App = () => {
-  
   const [stateNumb, setStateNumb] = useState(data);
-  const [stateValue, setStateValue] = useState([]);
+  const [stateOperation, setStateOperation] = useState(operation);
+  //const [stateValueNumber, setStateValueNumber] = useState("");
+  const [stateValueOperation, setStateValueOperation] = useState("");
+  const [stateValue, setStateValue] = useState("");
+  const [result, setResult] = useState(0);
+  const [firstNumber, setFirstNumber] = useState("");
+  const [secondNumber, setSecondNumber] = useState("");
 
-  const handleSubmit=(e)=>{
-    e.preventDefault()
-    console.log(stateValue)
-  }
+  //const handleSubmit = (e) => {
+    //e.preventDefault();
+    //console.log(stateValue);
+  //};
 
-  const handleGetNumber = (item)=>{
-    //setState(item)
-  
-    setStateValue(item
+  const handleGetNumber = (item) => {
+    if (stateValueOperation === "") {
+      setFirstNumber(firstNumber + item);
+    } else {
+      setSecondNumber(secondNumber + item);
+     // setStateValueOperation("")
+    }
+  };
+  console.log(`stateValueOperation${stateValueOperation}`)
+  console.log(firstNumber);
+  console.log(secondNumber);
+
+  const handleClickOperation = (item) => {
+    //setOperationState(item)
+    setStateValueOperation(item);
+  };
+  console.log(stateValueOperation);
+
+  const handleClickResult = () => {
+    switch (stateValueOperation) {
+      case "+":
+        return (
+          
+          setResult(Number(firstNumber) + Number(secondNumber))
+          
+        )
+      case "-":
+        return setResult(Number(firstNumber) - Number(secondNumber));
+        
+      case "*":
+        setResult(Number(firstNumber) * Number(secondNumber));
+        break;
+      case "/":
+        setResult(Number(firstNumber) / Number(secondNumber));
+        //refrechResult()
+        break;
+
+
+      default:
+        return null;
+    }
+    setStateValueOperation("")
     
-      )
-    console.log(stateValue);
-  }
+  };
+  console.log(result);
+  console.log(stateValueOperation)
+
+const refrechResult = ()=>{
+  setResult(0)
+  setStateValueOperation('')
+  setFirstNumber('')
+  setSecondNumber('')
+}
 
   return (
     <div className="App">
       <h1>React App Calculator</h1>
 
       <div className="container">
-        <form className="form" onSubmit={handleSubmit}>
-          <input
-            type="text"
-            name="stateValue"
-            value={stateValue}
-            required
-            placeholder="0"
-            onChange={(e)=>setStateValue(e.target.value)}
-          />
-        </form>
+        <div>{result}</div>
+       
         <div className="calculator">
           {/*<Number  setStateValue={setStateValue} stateValue={stateValue}/>*/}
           <div className="number">
-      {stateNumb.map((item, index) => (
-        <h3 className={index !== 0 ? "buttonNumb" : "buttonZero"}
-         key={index}
-         onClick={()=>handleGetNumber(item)}
-         >
-          {item}
-        </h3>
-      ))}
-    </div>
+            {stateNumb.map((item, index) => (
+              <h3
+                className={index !== 0 ? "buttonNumb" : "buttonZero"}
+                key={index}
+                onClick={() => handleGetNumber(item)}
+              >
+                {item}
+              </h3>
+            ))}
+          </div>
           {/* */}
-         <Operation setStateValue={setStateValue} stateValue={stateValue}/>
+          {/*<Operation setStateValue={setStateValue} stateValue={stateValue} />*/}
+
+          <div className="operation">
+            {stateOperation.map((item, index) => (
+              <h3
+                className="buttonOperation"
+                key={index}
+                onClick={() => handleClickOperation(item)}
+              >
+                {item}
+              </h3>
+            ))}
+            <>
+            <h3
+            style={{
+              backgroundColor: "red",
+              padding: "6px",
+              textAlign: "center",
+            }}
+            onClick={refrechResult}
+            >
+              Del
+            </h3>
+            <h3
+              style={{
+                backgroundColor: "red",
+                padding: "6px",
+                textAlign: "center",
+              }}
+              onClick={handleClickResult}
+            >
+              =
+            </h3>
+            </>
+          </div>
         </div>
       </div>
     </div>
